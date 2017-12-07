179c03fc4e7d37d61ecb693a11685dacad7d1723
makarenko_cr <makarenko.cr@gmail.com>
2017-12-07 15:29:07 +0200

    #5 Unused argument has been deleted from tests.

--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -2,14 +2,14 @@ import pytest
 from apps.fibonacci_app.app import get_fibonacci_sequence
 
 test_data = [
-    ("Get fibonacci sequence up to 7.", 7, [0, 1, 1, 2, 3, 5, 8]),
-    ("Input number is 0.", 0, []),
-    ("Input is -7.", -7, [])
+    pytest.param(7, [0, 1, 1, 2, 3, 5, 8], id='Get fibonacci sequence up to 7.'),
+    pytest.param(0, [], id='Input number is 0.'),
+    pytest.param(-7, [], id='Input is -7.')
 ]
 
 
-@pytest.mark.parametrize('desc, input, expected', test_data, ids=[desc[0] for desc in test_data])
+@pytest.mark.parametrize('input, expected', test_data)
 @pytest.mark.fib
-def test_get_fibonacci_sequence(desc, input, expected):
+def test_get_fibonacci_sequence(input, expected):
     actual_list = get_fibonacci_sequence(input)
     assert actual_list == expected
--- tests/test_numbers.py
+++ tests/test_numbers.py
@@ -2,14 +2,14 @@ import pytest
 from apps.numbers_app.app import get_pairs_of_numbers
 
 test_data = [
-    ("Positive test. Input in 10.", 10, 5),
-    ("Negative test. Input is -10.", -10, 0),
-    ("Negative test. Input is list.", [], 0)
+    pytest.param(10, 5, id='Positive test. Input in 10.'),
+    pytest.param(-10, 0, id='Negative test. Input is -10.'),
+    pytest.param([], 0, id='Negative test. Input is list.')
 ]
 
 
-@pytest.mark.parametrize('desc, input, expected', test_data, ids=[desc[0] for desc in test_data])
+@pytest.mark.parametrize('input, expected', test_data)
 @pytest.mark.numbers
-def test_get_pairs_of_numbers(desc, input, expected):
+def test_get_pairs_of_numbers(input, expected):
     actual = get_pairs_of_numbers(input)
     assert len(actual) == expected
