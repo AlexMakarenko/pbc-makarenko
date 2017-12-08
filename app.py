6610ca57bbb2c115113478c1e1a53300a79adfa1
makarenko_cr <makarenko.cr@gmail.com>
2017-12-08 20:23:28 +0200

    Changed description for app.

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from apps import get_fibonacci_sequence
 from apps import get_pairs_of_numbers
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="This app returns the fibonacci sequence of required length.",
+    parser = argparse.ArgumentParser(description="This app can return the fibonacci sequence of required length and"
+                                                 " parse numbers to return pairs with sum 10.",
                                      prog="PROG")
     group = parser.add_argument_group("Parameters")
-    parser.add_argument('-a', '--app', type=str, required=True, help='App to run: fib or pairs')
+    parser.add_argument('-a', '--app', type=str, required=True, help='App to run: fib or pairs.')
     group.add_argument("--length", "-l", action='store', help="Needed length of fibonacci sequence.", type=int,
                        required=False)
     group.add_argument("--numbers", "-n", action='store', nargs='+', help="Numbers separated by a space.", type=int,
