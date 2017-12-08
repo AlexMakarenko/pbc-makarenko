4e4e26584130cee68ad581f2fb5f29fdefca6052
makarenko_cr <makarenko.cr@gmail.com>
2017-12-08 19:53:51 +0200

    Removed unneeded variable.

--- apps/logger.py
+++ apps/logger.py
@@ -1,11 +1,10 @@
 def log(func):
     def wrapper(*args):
-        func_name = func.__name__
         list_of_args = []
         for a in args:
             list_of_args.append(a)
         rs = func(*args)
-        print('\n{}({})'.format(func_name, str(list_of_args).strip('[]')))
+        print('\n{}({})'.format(func.__name__, str(list_of_args).strip('[]')))
         return rs
 
     return wrapper
