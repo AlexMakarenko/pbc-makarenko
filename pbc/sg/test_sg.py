7e961feb5b2b28557816c548fd5341b8d58ba85f
makarenko_cr <makarenko.cr@gmail.com>
2017-12-13 12:35:27 +0200

    Added rest test for Selenium Grid. `test_sg_rest`

--- pbc/sg/test_sg.py
+++ pbc/sg/test_sg.py
@@ -1,5 +1,7 @@
 from pbc.sg import Grid
 from time import sleep
+import requests
+from bs4 import BeautifulSoup
 
 
 def test_processes(ssh_cl):
@@ -27,3 +29,15 @@ def test_sg_ui(browser, ssh_cl):
     sleep(2)
     browser.execute_script('location.reload();')
     assert len(browser.xpath('//img[contains(@src, "firefox")]', wait=True, ttl=10)) == 5
+
+
+def test_sg_rest(ssh_cl):
+    grid = Grid(ssh_cl)
+    grid.download()
+    grid.start_hub()
+    grid.add_node()
+    sleep(5)
+    r = requests.get('http://192.168.33.10:4444/grid/console')
+    soup = BeautifulSoup(r.text, 'html.parser')
+    firefox_browsers = [x for x in soup.find_all('img') if str(x['src']).endswith('firefox.png')]
+    assert len(firefox_browsers) == 5
